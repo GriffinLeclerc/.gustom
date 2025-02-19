9fa4a93595604395b6135b63c3954e566661ade8
griffin <leclercgriffin@gmail.com>
2025-02-18 20:30:57 -0500

    new bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.shell.extensions.ding show-home false
 sudo apt install xclip
